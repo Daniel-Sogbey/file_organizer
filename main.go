df014f7d848718d1c7ac8a3a0d8a28f6d61eb586
DaNiEl SoGbEy <daniel.sogbey@hubtel.com>
2025-03-12 21:54:37 +0000

    added a readme file and code refatoring

--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func main() {
 	flag.StringVar(&dir, "dir", "", "directory that has the files needing organization")
 	flag.Parse()
 
+	if dir == "" {
+		fmt.Println("Specify a directory")
+		return
+	}
+
 	fmt.Println("Directory:", dir)
 
 	err := organizeFiles(dir)
